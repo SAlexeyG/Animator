08cf6049136ed11019340462422b49d49926522e
Alexey Smyshlaev <alexeysm@playtika.com>
2020-10-27 14:09:14 +0300

    no message

---
+++ Assets/Standard Assets/2D/Scripts/RobotScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RobotScript : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) animator.SetTrigger("Jump");
+        animator.SetBool("IsWalking", Input.GetKey(KeyCode.D));
+        animator.SetBool("IsCrouching", Input.GetKey(KeyCode.LeftControl));
+    }
+}
