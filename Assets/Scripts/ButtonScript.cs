72867f3afd4b71d729e2b420d1c1010b0b7b4413
Alexey Smyshlaev <alexeysm@playtika.com>
2020-10-27 18:20:18 +0300

    BlendTree

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    public void ParticelSystemPlaying()
+    {
+        GetComponent<ParticleSystem>().Play();
+    }
+
+    public void Click()
+    {
+        animator.SetFloat("Blend", Random.Range(0f, 1f));
+        animator.SetTrigger("Click");
+    }
+}
